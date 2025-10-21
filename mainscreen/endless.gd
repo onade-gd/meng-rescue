3ada6a851fe72438da26e46412cd2be18be3e50a
demone <hagioslukassianipar@gmail.com>
2025-10-22 01:56:30 +0700

    18-19/10/2025

--- mainscreen/endless.gd
+++ mainscreen/endless.gd
@@ -3,7 +3,18 @@ extends Control
 @export var rarity : int
 var stage = preload("res://resource/stage_resource.tres")
 var score : int = 0 
-var chosen_stage
+var chosen_stage : Array = [
+	load("res://stages endless/0.tscn"),
+	load("res://stages endless/1.tscn"),
+	load("res://stages endless/2.tscn"),
+	load("res://stages endless/3.tscn"),
+	load("res://stages endless/4.tscn"),
+	load("res://stages endless/5.tscn"),
+	load("res://stages endless/6.tscn"),
+	load("res://stages endless/7.tscn"),
+	load("res://stages endless/8.tscn"),
+	load("res://stages endless/9.tscn")
+]
 var tween
 var next_stage
 var spawn = 0
@@ -11,13 +22,15 @@ func _ready() -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:
+	print($StageParent.get_child(-1))
 	if $StageParent.get_child_count() < 3:
-		chosen_stage = $StageParent.get_child(-1).get_meta("next").pick_random()
-		spawn = spawn + $StageParent.get_child(-1).get_meta("end")
-		next_stage = chosen_stage.instantiate()
+		next_stage = chosen_stage[$StageParent.get_child(-1).get_meta("next").pick_random()].instantiate()
+		spawn = spawn - 2400
 		$StageParent.add_child(next_stage)
 		next_stage.position = Vector2(0,spawn)
-
+	if $StageParent.get_child(-1).global_position.y == -4960:
+		$StageParent.get_child(-1).queue_free()
+		
 func _on_main_char_hp(value) -> void:
 	if tween:
 		tween.kill
