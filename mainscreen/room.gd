74f0ec6fcc267c9d27524728bbb4b147f3e042b8
demone <hagioslukassianipar@gmail.com>
2025-11-14 17:27:32 +0700

    14/11/2025

--- mainscreen/room.gd
+++ mainscreen/room.gd
@@ -2,15 +2,27 @@ extends Control
 var new_placement
 var tween
 var furniture_name
+var confirmable: bool
+var cat = 3
 func _ready() -> void:
 	for furnitures in PlayerprogressSavefile.rooms[0]:
-		print(furnitures)
 		$FurnitureContainer.add_child(load(furnitures[0]["furniture"]).instantiate())
 		$FurnitureContainer.get_child(-1).position = Vector2(furnitures[0]["pos"])
+	while cat > 0:
+		var step1 = randi_range(0, $FurnitureContainer.get_child_count()-1)
+		var step2 = randi_range(0,$FurnitureContainer.get_child(step1).get_child(2).get_child_count()-1)
+		$FurnitureContainer.get_child(step1).get_child(2).get_child(step2).add_child(preload("res://extra assets/Furnitures/furniture_cat.tscn").instantiate())
+		PlayerprogressSavefile.rooms[0][step1]["cat_spawn"][step2] = 1
+		
 	set_physics_process(false)
 func _physics_process(_delta: float) -> void:
+	if $FurnitureQueue.get_child(0).get_child(0).get_overlapping_areas() == []:
+		$FurnitureQueue.get_child(0).set_modulate(Color.WHITE)
+		confirmable = true
+	else:
+		confirmable = false
+		$FurnitureQueue.get_child(0).set_modulate(Color.RED)
 	snap_furnitures_to_floor()
-	
 	if Input.is_action_just_released("touch"):
 		set_physics_process(false)
 func _on_furn_button_down(furniture) -> void:
@@ -18,6 +30,7 @@ func _on_furn_button_down(furniture) -> void:
 	new_placement = load(str(furniture))
 	furniture_name = furniture
 	$FurnitureQueue.add_child(new_placement.instantiate())
+	$FurnitureQueue.get_child(0).get_child(0).area_entered.connect(_area_entered_room)
 	if tween:
 		tween.kill
 	tween = create_tween()
@@ -41,15 +54,24 @@ func _on_cancel_pressed() -> void:
 	$FurnitureQueue.get_child(0).queue_free()
 	$Panel.visible = false
 
+func _area_entered_room(area):
+	confirmable = false
+
 func _on_confirm_pressed() -> void:
-	$FurnitureQueue.get_child(0).queue_free()
-	$FurnitureContainer.add_child(new_placement.instantiate())
-	$FurnitureContainer.get_child(-1).position  = $FurnitureQueue.get_child(0).position
-	PlayerprogressSavefile.rooms[0].append([{"furniture":str(furniture_name), "pos":$FurnitureContainer.get_child(-1).position}])
-	PlayerprogressSavefile.save_data()
-	if tween:
-		tween.kill
-	tween = create_tween()
-	tween.tween_property($VBoxContainer/TabContainer,"size_flags_stretch_ratio",0.75,0.1)
-	$Panel.visible = false
-	print(PlayerprogressSavefile.rooms[0])
+	if confirmable:
+		var new_furniture_cat_spawn : Array
+		new_furniture_cat_spawn.resize(4)
+		for i in range(new_furniture_cat_spawn.size()):
+			new_furniture_cat_spawn[i] = 0
+		print(new_furniture_cat_spawn)
+		$FurnitureQueue.get_child(0).queue_free()
+		$FurnitureContainer.add_child(new_placement.instantiate())
+		$FurnitureContainer.get_child(-1).position  = $FurnitureQueue.get_child(0).position
+		PlayerprogressSavefile.rooms[0].append([{"furniture":str(furniture_name), "pos":$FurnitureContainer.get_child(-1).position, "cat_spawn": new_furniture_cat_spawn}])
+		PlayerprogressSavefile.save_data()
+		if tween:
+			tween.kill
+		tween = create_tween()
+		tween.tween_property($VBoxContainer/TabContainer,"size_flags_stretch_ratio",0.75,0.1)
+		$Panel.visible = false
+		print(PlayerprogressSavefile.rooms[0])
